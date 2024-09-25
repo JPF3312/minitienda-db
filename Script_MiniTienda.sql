46643c044e47d92212273a317060c668983f8386
Johan Patiño Fernandez <patinofernandezjohanbladimir@gmail.com>
2024-09-25 10:52:45 -0500

    se crea el scrip con los sp de la Mini Tienda, de la tabla proveedores

--- Script_MiniTienda.sql
+++ Script_MiniTienda.sql
@@ -9,7 +9,12 @@ DELIMITER ;
 
 -- Actualizar
 DELIMITER //
-
+create procedure procUpdateCategory(IN v_id INT, IN v_description VARCHAR(45))
+begin
+	update tbl_categorias
+    set cat_descripcion = v_description
+    where cat_id = v_id;
+end//
 DELIMITER ;
 
 -- Mostrar
@@ -22,9 +27,57 @@ DELIMITER ;
 
 -- Eliminar 
 DELIMITER //
-
+create procedure procDeleteCategory(IN v_id INT)
+begin
+	delete from tbl_categorias where cat_id = v_id;
+end//
 DELIMITER ;
 
 -- Proveedores
+
+
+-- Insertar
+DELIMITER //
+create procedure procInsertProviders(IN v_nit VARCHAR (45), IN v_nombre VARCHAR(100))
+begin
+	INSERT INTO tbl_proveedores(prov_nit) values(v_nit, v_nombre);
+end//
+DELIMITER ;
+
+
+
+-- Mostrar
+DELIMITER //
+create procedure procSelectProviders()
+begin
+	select prov_id,prov_nit,prov_nombre from tbl_proveedores;
+end//
+DELIMITER ;
+
+
+
+
+-- Eliminar 
+DELIMITER //
+create procedure procDeleteProviders(IN v_id INT)
+begin
+	delete from tbl_proveedores where prov_id = v_id;
+end//
+DELIMITER ;
+
+
+
+-- Actualizar
+DELIMITER //
+create procedure procUpdateProviders(IN v_id INT, IN v_nit VARCHAR(45), IN v_nombre VARCHAR (100))
+begin
+	update tbl_proveedores
+    set prov_nit = v_nit , prov_nombre = v_nombre
+    where prov_id = v_id;
+end//
+DELIMITER ;
+
+
+
 -- Productos
 -- Usuarios
